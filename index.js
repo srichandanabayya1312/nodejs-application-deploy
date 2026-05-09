df40d697300081f0a3d1da11013145ae109fc50c
Sri Chandana <srichandanabayya@gmail.com>
2026-05-09 14:25:17 +0000

    updated workflows

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello from the server v2'})
+    return res.json({ msg: 'Hello from the server v6'})
 })
 
 app.listen(PORT, () => {
